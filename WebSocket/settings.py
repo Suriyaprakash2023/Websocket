a6af124b7ee9ecaac155416c1373727693034f51
Suriya Prakash <150240754+Suriyaprakash2023@users.noreply.github.com>
2024-11-30 07:49:51 +0530

    debug True

--- WebSocket/settings.py
+++ WebSocket/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c^nxqc&96#=t2imn^l=z31r745%xy^qlmvck^boxrd%lqw#!i*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*",]
 
