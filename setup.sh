3660acebc2b87a026adeb04bb78216855f68e19d
Suriya Prakash <150240754+Suriyaprakash2023@users.noreply.github.com>
2024-12-01 07:24:08 +0530

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run migrations to set up the database
+ python manage.py collectstatic --noinput
+#python manage.py makemigrations --noinput
+python manage.py migrate
+#if [[ $CREATE_SUPERUSER ]];
+#then
+#  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+# Collect static files (optional, remove if not using static files)
+# python manage.py collectstatic --noinput
+
+# You do not need to start the Django server with `runserver` on Vercel.
