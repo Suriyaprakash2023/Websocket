77553de9abb7a57eb92195baa3c04bfc168592be
Suriya Prakash <150240754+Suriyaprakash2023@users.noreply.github.com>
2024-12-01 09:12:51 +0530

    import user model chamge

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,8 +2,9 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer
 import json
 # from . models import MyChats
 # from django.contrib.auth.models import User
-from django.contrib.auth import get_user_model
-User = get_user_model()
+# from django.contrib.auth import get_user_model
+# User = get_user_model()
+from django.contrib.auth.models import User
 from time import sleep
 import datetime
 from channels.db import database_sync_to_async
